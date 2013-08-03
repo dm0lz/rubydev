ee4e9f4e5dd77bcacb6d5a5bc0c5e9492a9d69b1
OlIvIeR77 <ducrouxolivier@gmail.com>
2013-08-03 17:09:18 +0200

    first commit

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+require "bundler/capistrano"
+require "rvm/capistrano"
+
+load "config/recipes/base"
+load "config/recipes/node"
+load "config/recipes/nginx_puma"
+load "config/recipes/puma"
+load "config/recipes/mysql"
+load "config/recipes/rvm"
+
+server "rubydev.fr", :web, :app, :db, primary: true
+
+set :site_url, "rubydev.fr"
+set :application, "rubydev"
+set :user, "olivierdoe"
+
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :use_sudo, false
+
+set :scm, "git"
+set :repository,  "https://github.com/oducroux/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubydev::Application.config.secret_key_base = '8deeed5e67778c6f9124abe19f6ed49cd3091cf93e5cefc47abe8cafdf241354e27e405f6267372805ca53ee83a325c9d0ca07f09298fc3d7122e6ba5e408543'
